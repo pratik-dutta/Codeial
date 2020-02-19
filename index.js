4bab4af098ad7024fd58d6ee4d24ddfbded02d7f
pratik-dutta <pratikdutta.786@gmail.com>
2020-02-19 16:54:01 +0530

    setup ejs view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 //use exress router
 app.use("/", require("./routes/index"));
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function(err){
     if(err){
         console.log("Error : ", err);
