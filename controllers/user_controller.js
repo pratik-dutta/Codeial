f645cf9ecd2e7b4f55f4bda98ec9ee3c895869a3
pratik-dutta <pratikdutta.786@gmail.com>
2020-02-25 14:19:48 +0530

    added signin and signup layout in user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,3 +9,16 @@ module.exports.post = function(req, res){
         title: "User Post"
     });
 };
+
+module.exports.signup = function(req, res){
+    return res.render("signup", {
+        title: "User SignUp"
+        
+    });
+};
+
+module.exports.signin = function(req, res){
+    return res.render("signin", {
+        title: "User SignIn"
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ const userController = require("../controllers/user_controller");
 
 router.get("/profile", userController.profile);
 router.get("/post", userController.post);
+router.get("/signup", userController.signup);
+router.get("/signin", userController.signin);
 
 module.exports = router;
