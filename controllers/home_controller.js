bff0f84cb9050681536f735969f9198ea681908b
pratik-dutta <pratikdutta.786@gmail.com>
2020-02-28 16:41:22 +0530

    created functianilty to show post and it's username

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req, res){
     
     //to check the cookies made in chrome
@@ -6,7 +8,33 @@ module.exports.home = function(req, res){
     //to set the cookie
     // res.cookie("user_id", 25);
     
-    return res.render("home", {
-        title: "Home"
-    });
+
+    //it does not populate the user
+    // Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log("Error in fetching Posts");
+    //         return;
+    //     }
+    //     return res.render("home", {
+    //         title: "Home",
+    //         posts: posts,
+            
+    //     });
+    // });
+
+    //it populate the user
+    Post.find({}).populate("user").exec(function(err, posts){
+            if(err){
+                console.log("Error in fetching Posts");
+                return;
+            }
+            return res.render("home", {
+                title: "Home",
+                posts: posts,
+                
+            });
+        }
+    );
+
+    
 }
