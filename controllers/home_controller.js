1cd352171079a7fd7872110ee8a884edba092d2c
pratik-dutta <pratikdutta.786@gmail.com>
2020-03-03 17:53:03 +0530

    added async feature in controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,69 +1,100 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function(req, res){
+//using Async
+module.exports.home = async function(req, res){
+
+    try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
     
-    //to check the cookies made in chrome
-    // console.log(req.cookies);
+        let users = await User.find({});
 
-    //to set the cookie
-    // res.cookie("user_id", 25);
-    
-
-    //it does not populate the user
-    // Post.find({}, function(err, posts){
-    //     if(err){
-    //         console.log("Error in fetching Posts");
-    //         return;
-    //     }
-    //     return res.render("home", {
-    //         title: "Home",
-    //         posts: posts,
-            
-    //     });
-    // });
-
-    //it populate the user
-    // Post.find({}).populate("user").exec(function(err, posts){
-    //         if(err){
-    //             console.log("Error in fetching Posts");
-    //             return;
-    //         }
-    //         return res.render("home", {
-    //             title: "Home",
-    //             posts: posts,
-                
-    //         });
-    //     }
-    // );
-
-    // it populate the user as well as the comments and it's user
-
-    Post.find({})
-    .populate("user")
-    .populate({
-        path: "comments",
-        populate: {
-            path: "user"
-        }
-    })
-    .exec(function(err, posts){
-        if(err){
-            console.log("Error in fetching Posts");
-            return;
-        }
-
-        User.find({}, function(err, users){
-            return res.render("home", {
-                title: "Home",
-                posts: posts,
-                all_users: users,
-            });
+        return res.render('home', {
+            title: "Home",
+            posts:  posts,
+            all_users: users
         });
 
-        
+    }catch(err){
+        console.log('Error', err);
+        return;
     }
-);
+   
+}
+
+
+//causing callback hell
+// module.exports.home = function(req, res){
+    
+//     //to check the cookies made in chrome
+//     // console.log(req.cookies);
+
+//     //to set the cookie
+//     // res.cookie("user_id", 25);
+    
+
+//     //it does not populate the user
+//     // Post.find({}, function(err, posts){
+//     //     if(err){
+//     //         console.log("Error in fetching Posts");
+//     //         return;
+//     //     }
+//     //     return res.render("home", {
+//     //         title: "Home",
+//     //         posts: posts,
+            
+//     //     });
+//     // });
+
+//     //it populate the user
+//     // Post.find({}).populate("user").exec(function(err, posts){
+//     //         if(err){
+//     //             console.log("Error in fetching Posts");
+//     //             return;
+//     //         }
+//     //         return res.render("home", {
+//     //             title: "Home",
+//     //             posts: posts,
+                
+//     //         });
+//     //     }
+//     // );
+
+//     // it populate the user as well as the comments and it's user
+
+//     Post.find({})
+//     .populate("user")
+//     .populate({
+//         path: "comments",
+//         populate: {
+//             path: "user"
+//         }
+//     })
+//     .exec(function(err, posts){
+//         if(err){
+//             console.log("Error in fetching Posts");
+//             return;
+//         }
+
+//         User.find({}, function(err, users){
+//             return res.render("home", {
+//                 title: "Home",
+//                 posts: posts,
+//                 all_users: users,
+//             });
+//         });
+
+        
+//     }
+// );
 
     
-}
+// }
