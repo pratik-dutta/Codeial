b5515dba703a729e50d2023af9bb2661daa4b6c5
pratik-dutta <pratikdutta.786@gmail.com>
2020-03-11 21:43:28 +0530

    added feature (delayed Jobs using Kue)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,10 @@
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const commentsMailer = require("../mailers/comments_mailer");
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+
+
 
 //with async
 
@@ -21,7 +25,17 @@ module.exports.create = async function(req, res){
 
             // Similar for comments to fetch the user's id!
             comment = await comment.populate('user', 'name email').execPopulate();
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
+
+            });
+
 
             if (req.xhr){
                 
